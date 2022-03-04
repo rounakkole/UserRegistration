e04e1251417ddc9b3c875a8a7e429c6877c02481
rounak <rounakkole@gmail.com>
2022-03-04 18:45:37 +0530

    [rounak] ADDED: check all email samples

--- Program.cs
+++ Program.cs
@@ -9,10 +9,25 @@ namespace UserRegistration
         {
             Console.WriteLine("user registration");
             RegistrationPattern registrationPattern = new RegistrationPattern();
-            registrationPattern.Validate("91720654@9hjnA");
-            registrationPattern.Validate("abcdefhi");
-            registrationPattern.Validate("abcDE1234");
-            registrationPattern.Validate("abCD@12");
+            //valid
+            registrationPattern.Validate("abc@yahoo.com");
+            registrationPattern.Validate("abc-100@yahoo.com");
+            registrationPattern.Validate("abc.100@yahoo.com");
+            registrationPattern.Validate("abc111@abc.com");
+            registrationPattern.Validate("abc-100@abc.net");
+            registrationPattern.Validate("abc.100@abc.com.au");
+            registrationPattern.Validate("abc@1.com");
+            registrationPattern.Validate("abc@gmail.com.com");
+            registrationPattern.Validate("abc+100@gmail.com");
+            //invalid
+            registrationPattern.Validate("abc@.com.my");
+            registrationPattern.Validate(".abc@abc.com");
+            registrationPattern.Validate("abc@abc@gmail.com");
+            registrationPattern.Validate("abc..2002@gmail.com");
+            registrationPattern.Validate("abc@gmail.com.1a");
+            registrationPattern.Validate("abc@gmail.com.aa.au");
+            
+
         }
     }
 }
--- RegistrationPattern.cs
+++ RegistrationPattern.cs
@@ -14,12 +14,12 @@ namespace UserRegistration
         public static string email = "^[a-z]{3,}([.]{1}[a-z]{2,})?[/@/][a-z]{2,5}[/./][a-z]{2,5}([.]{1}[a-z]{2})?$";
         public static string mobileNo = "^91[/ /][6-9]{1}[0-9]{9}$";
         public static string password = "^(?=.*[!@#$%^&*]{1})(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9@$!%*#?&]{8,}$";
-
+        public static string UC9 = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
         public void Validate(string data)
         {
-                string result = Regex.IsMatch(data, password) ? $"{data}: valid" : $"{data}: invalid";
+            string result = Regex.IsMatch(data, UC9) ? $"{data}: valid" : $"{data}: invalid";
 
-                Console.WriteLine(result);
+            Console.WriteLine(result);
         }
     }
 }

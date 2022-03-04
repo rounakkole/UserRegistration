b86afd2c7fc5fb8324218fdd40e6b154dfea3607
rounak <rounakkole@gmail.com>
2022-03-04 13:31:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UserRegistration
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("user registration");
+        }
+    }
+}
